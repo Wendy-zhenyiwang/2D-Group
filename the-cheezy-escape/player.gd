0b3b559a582b157771e858242ec7e6804ae1f597
yzhang134 <yzhang134@sva.edu>
2024-10-28 16:10:19 -0400

    update

--- the-cheezy-escape/player.gd
+++ the-cheezy-escape/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed = 5
 
@@ -12,22 +12,22 @@ func _process(delta):
 	
 	
 	if Input.is_action_pressed("ui_left"):
-		position.x -= speed
+		velocity.x -= speed
 		$annimation.play("walk_left")
 		
 	elif Input.is_action_pressed("ui_right"):
-		position.x += speed
+		velocity.x += speed
 		$annimation.play("walk_right")
 		
 	elif Input.is_action_pressed("ui_up"):
-		position.y -= speed
+		velocity.y -= speed
 		
 	elif Input.is_action_pressed("ui_down"):
-		position.y += speed
+		velocity.y += speed
 		
 	else:
 		$annimation.play("idle")
-		
+	move_and_slide()
 		
 		
 		
