bc2214b07539de5101bd7846e59b77369c436ce4
yzhang134 <yzhang134@sva.edu>
2024-10-28 17:28:38 -0400

    1

--- the-cheezy-escape/platformer player.gd
+++ the-cheezy-escape/platformer player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 5
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1000.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,8 +11,6 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,18 +19,26 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= speed
-		$annimation.play("walk_left")
+		if is_on_floor():
+			$annimation.play("walk_left")
 	
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x += speed
-		$annimation.play("walk_right")
+		if is_on_floor():
+			$annimation.play("walk_right")
+		
 	else:
-		$annimation.play("idle")
+		if is_on_floor():
+			$annimation.play("idle")
 	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 		
+		$annimation.play("jump_up_left")
 
 	move_and_slide()
